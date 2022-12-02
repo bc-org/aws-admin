d357796e8972fb289f7baa39142cb97ae2dbb747
tejas <tejas.morbagalharish@brockmann-consult.de>
2022-12-02 13:58:36 +0100

    refactor

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-import setuptools
-setuptools.setup()
+from setuptools import setup, find_packages
+packages = find_packages(exclude=["test", "test.*"])
+version = None
+with open('aws_admin/version.py') as f:
+    exec(f.read())
+setup(
+    name="aws_admin",
+    version=version,
+    packages=packages
+)
